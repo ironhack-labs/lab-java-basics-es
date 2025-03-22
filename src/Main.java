a9a535d16a0490748a9a06de8e45d80708b603a5
lisamedina <lisa.medinag@gmail.com>
2025-03-22 15:05:03 +0100

    lab finished

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Resuelvo mi lab");
+
+        differenceBetweenSmallerAndLarger();
+        smallestAndSecondSmallest();
+        create10Employees();
+    }
+
+    public static int differenceBetweenSmallerAndLarger(){
+        return 1;
+    }
+
+    public static int smallestAndSecondSmallest(){
+        return 1;
+    }
+
+    public static void create10Employees(){
+    }
+}
